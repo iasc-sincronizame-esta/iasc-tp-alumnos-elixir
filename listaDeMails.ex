fd01cbedcf2547939003a9e2febf5a2d7f5f85f9
Rodrigo Alfonso <rodri042@gmail.com>
2015-10-27 20:07:54 -0300

    Los alumnos pueden mandar consultas

--- listaDeMails.ex
+++ listaDeMails.ex
@@ -3,21 +3,45 @@ require Integer
 defmodule ListaDeMails do
   def start() do
     suscriptores = []
-    spawn fn -> loop(suscriptores) end
+    consultas = []
+    spawn fn -> loop({ suscriptores, consultas }) end
   end
 
-  def loop(suscriptores) do
+  def loop({ suscriptores, consultas }) do
     receive do
-      { :suscribir, suscriptor, pid } ->
-        loop(suscriptores ++ [ { suscriptor, pid } ])
+      { :suscribir, pidSuscriptor } ->
+        IO.puts "Se suscribió alguien"
+        suscriptores = suscriptores ++ [ pidSuscriptor ]
+        loop({ suscriptores, consultas})
 
       { :listar, pid } ->
         send pid, suscriptores
-        loop(suscriptores)
+        loop({ suscriptores, consultas})
+
+      { :consultar, consulta, pidAlumno } ->
+        IO.puts "Están consultando #{consulta}"
+        consultas = consultas ++ [ { consulta, pidAlumno } ]
+        loop({ suscriptores, consultas})
     end
   end
 end
 
 # ---
 
-a = ListaDeMails.start()
+defmodule Alumno do
+  def start() do
+    spawn fn -> loop() end
+  end
+
+  def loop() do
+    receive do
+      { :consultar, lista, consulta } ->
+        send lista, { :consultar, consulta, self }
+        loop()
+    end
+  end
+end
+
+laLista = ListaDeMails.start()
+unAlumno = Alumno.start()
+send unAlumno, { :consultar, laLista, "¿Cuál es el sentido de la vida?" }
