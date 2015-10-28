5648ffbba504e72b6daf291c418649cb7f9301ae
Rodrigo Alfonso <rodri042@gmail.com>
2015-10-27 21:38:56 -0300

    Ahora los docentes pueden responder consultas

--- listaDeMails.ex
+++ listaDeMails.ex
@@ -21,12 +21,31 @@ defmodule ListaDeMails do
 
       { :consultar, consulta, pidAlumno } ->
         IO.puts "Están consultando #{consulta}"
-        consultas = consultas ++ [ { consulta, pidAlumno } ]
+        idConsulta = make_ref
+        consultas = consultas ++ [ { idConsulta, consulta, pidAlumno, [] } ]
 
         Enum.each suscriptores, fn it ->
           if it != pidAlumno do
-            send it, { :notificacion_consulta, { consulta, pidAlumno } }
+            send it, { :notificacion_consulta, { idConsulta, consulta, pidAlumno } }
           end
+        end
+
+      { :responder, idConsulta, respuesta } ->
+        esLaConsultaQuePiden = fn it ->
+          (elem it, 0) == idConsulta
+        end
+
+        consulta = Enum.find consultas, esLaConsultaQuePiden
+
+        if consulta != nil do
+          IO.puts "Están respondiendo la consulta #{elem consulta, 1} con la respuesta #{respuesta}"
+          respuestas = elem consulta, 3
+          consulta = put_elem consulta, 3, respuestas ++ [ respuesta ]
+          
+          consultas = Enum.reject consultas, esLaConsultaQuePiden
+          consultas = consultas ++ [ consulta ]
+        else
+          IO.puts "Quieren responder una consulta, pero la re flashearon con el id"
         end
     end
 
@@ -47,16 +66,52 @@ defmodule Alumno do
         send lista, { :consultar, consulta, self }
         loop()
 
-      { :notificacion_consulta, { consulta, pidAlumno } } ->
+      { :notificacion_consulta, { id, consulta, pidAlumno } } ->
         IO.puts "Se ve que alguien preguntó #{consulta}"
         loop()
     end
   end
 end
 
+# ---
+
+defmodule Docente do
+  def start() do
+    spawn fn -> loop() end
+  end
+
+  def loop() do
+    receive do
+      { :responder, lista, idConsulta, respuesta } ->
+        send lista, { :responder, idConsulta, respuesta }
+        loop()
+
+      { :notificacion_consulta, { id, consulta, pidAlumno } } ->
+        IO.puts "Se ve que alguien preguntó #{consulta}, estaría para contestarle, ¿no?"
+        loop()
+    end
+  end
+end
+
+# ---
+
 laLista = ListaDeMails.start()
+
 alumnoPreguntadorYSuscriptor = Alumno.start()
 alumnoSuscriptor = Alumno.start()
-send laLista, { :suscribir, alumnoSuscriptor }
+
 send laLista, { :suscribir, alumnoPreguntadorYSuscriptor }
 send alumnoPreguntadorYSuscriptor, { :consultar, laLista, "¿Cuál es el sentido de la vida?" }
+
+docente = Docente.start()
+
+# ver qué mensajes hay y contestar alguno...
+
+send laLista, { :listar_consultas, self }
+receive do
+  consultas ->
+    primera = List.first consultas
+    idDeLaPrimera = elem primera, 0
+
+    send docente, { :responder, laLista, idDeLaPrimera, "La respuesta está en tu corazón (?)" }
+end
